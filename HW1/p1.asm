09ffb3e614a78fe2f48192c7b307e2ee2abe4678
hentci <1222bruce@gmail.com>
2023-10-18 04:51:04 +0800

    [add] p1

--- HW1/p1.asm
+++ HW1/p1.asm
@@ -1,13 +1,14 @@
 section .bss
     name resb 256     ; Allocate memory space for the input file name
     result resb 1024
+
 section .data
-    path db "/mnt/c/code/SSS-HW/HW1/XDD/", 0
-    path_len equ $ - path
     prompt db "Enter the file name: ", 0
     prompt_len equ $ - prompt
+
 section .text
     global _start
+    
 _start:
     ; Output a prompt message, asking the user to enter a file name
     mov eax, 4       ; syscall number for write (STDOUT)
@@ -23,6 +24,25 @@ _start:
     mov edx, 256     ; maximum number of bytes to read
     int 0x80         ; interrupt to invoke the syscall (read)
 
+    ; Remove the newline character from the input
+    mov ecx, name    ; pointer to the name buffer
+    mov edx, 256     ; maximum number of bytes to check
+
+find_newline:
+    mov al, byte [ecx]  ; Load the current byte
+    cmp al, 10          ; Compare it to newline (ASCII 10)
+    je remove_newline   ; If it's a newline, jump to remove_newline
+    cmp al, 0           ; Compare it to null terminator (end of string)
+    je open_file        ; If it's null terminator, jump to open_file
+    inc ecx             ; Move to the next byte
+    dec edx             ; Decrement the count
+    jnz find_newline    ; Continue searching
+
+remove_newline:
+    mov byte [ecx], 0   ; Replace the newline with null terminator
+    jmp open_file       ; Jump to open_file
+
+open_file:
     ; Open or create a file
     mov eax, 5       ; syscall number for open
     mov ebx, name    ; pointer to the file name
